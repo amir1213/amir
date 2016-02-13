9efc7cd6c4d5523e99aba2add59309fb49c17735
teletamir <www.amir.amir1213@gmail.com>
2016-02-13 17:11:53 +0330

    Update auto_leave.lua

--- plugins/auto_leave.lua
+++ plugins/auto_leave.lua
@@ -1,28 +1,30 @@
 --[[
 Kicking ourself (bot) from unmanaged groups.
-When someone invited this bot to a group,  the bot will exit immediately by kicking itself out of that group.
-No switch available. You need to turn it on or off using !plugins command.
---]]
 
--- suppress '*** lua: attempt to call a nil value' warning
+When someone invited this bot to a group, the bot then will chek if the group is in its moderations (moderation.json). If not, the bot will exit immediately by kicking itself out of that group.
+
+No switch available. You need to turn it on or off using !plugins command.
+—]]
+
+— suppress '*** lua: attempt to call a nil value' warning
 local function callback(extra, success, result)
   vardump(success)
   vardump(result)
 end
 
 local function run(msg)
-  if msg.service and msg.action.type == 'chat_add_user' then
+  if msg.service and msg.action.type == 'chat_add_user' and  if not is_admin then
     local data = load_data(_config.moderation.data)
     if not data[tostring(msg.to.id)] then
       print "This is not our group. Leaving..."
-      chat_del_user('chat#id'..msg.to.id, 'user#id'..167946378, callback, false)
+      chat_del_user('chat#id'..msg.to.id, 'user#id'..our_id, callback, false)
     end
   end
 end
 
 return {
   description = "Kicking ourself (bot) from unmanaged groups.",
-  usage = "Plugin For Kiling add with id ....",
+  usage = "No switch available. Turn it on or off using !plugins command.",
   patterns = {
     "^!!tgservice (.+)$"
   },
